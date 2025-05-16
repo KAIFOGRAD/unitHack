15a7a972fb90a2991a87c3b4a46e96cb4ff9cc2f
Vladislav Ostanin <ostanin8900@yandex.ru>
2025-05-16 20:44:31 +0500

    add jakaratra valid + user import

---
+++ backend/src/main/java/com/nauHack/backend/entities/User/Role.java
@@ -0,0 +1,32 @@
+package com.nauHack.backend.entities.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+
+}
--- backend/src/main/java/com/nauHack/backend/entities/User/User.java
+++ backend/src/main/java/com/nauHack/backend/entities/User/User.java
@@ -1,5 +1,29 @@
 package com.nauHack.backend.entities.User;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
 
 }
