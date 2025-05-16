5120a9e5502419f0dc652b7c256ad76d1f013dae
sharkioso <tetkin04@mail.ru>
2025-05-16 21:52:54 +0500

    создан абстрактный класс отправки сообщений

---
+++ tgbot/src/main/java/com/example/Handlers/BaseHandler.java
@@ -0,0 +1,19 @@
+package com.example.Handlers;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public abstract class BaseHandler extends TelegramLongPollingBot {
+    public void sendMessage(Long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(textToSend);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            System.out.println("Uncorrected" + e);
+        }
+
+    }
+}
