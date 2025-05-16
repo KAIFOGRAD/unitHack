a108b8120b7f8d89dfcdf8f6f5a3cc12757341c4
Vladislav Ostanin <ostanin8900@yandex.ru>
2025-05-17 00:21:03 +0500

    add event entity

---
+++ backend/src/main/java/com/nauHack/backend/entities/Event.java
@@ -0,0 +1,42 @@
+package com.nauHack.backend.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "events")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Date startTime;
+
+    private Date endTime;
+
+    private int numberSeets;
+
+    private int maxSeets;
+
+    private String location;
+    
+
+}
