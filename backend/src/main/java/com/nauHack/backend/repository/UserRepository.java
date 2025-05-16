c0ddb0f63c8a815e21d2e42004532829c01b1995
Vladislav Ostanin <ostanin8900@yandex.ru>
2025-05-16 21:17:22 +0500

    add authToken

--- backend/src/main/java/com/nauHack/backend/repository/UserRepository.java
+++ backend/src/main/java/com/nauHack/backend/repository/UserRepository.java
@@ -1,11 +1,18 @@
 package com.nauHack.backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.nauHack.backend.entities.User.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 
 }
--- backend/src/main/java/com/nauHack/backend/service/UserDeatailServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.nauHack.backend.service;
-
-public class UserDeatailServiceImpl {
-
-}
