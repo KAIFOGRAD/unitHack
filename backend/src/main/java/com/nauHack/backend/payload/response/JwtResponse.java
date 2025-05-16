fc66a99ae9f2074d960aa76a66d862da4d8ff91e
Vladislav Ostanin <ostanin8900@yandex.ru>
2025-05-16 23:07:40 +0500

    add jwtResponse

---
+++ backend/src/main/java/com/nauHack/backend/payload/response/JwtResponse.java
@@ -0,0 +1,21 @@
+package com.nauHack.backend.payload.response;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private String id;
+    private String username;
+    private String email;
+    private String companyId;
+    private List<String> roles;
+    
+}
