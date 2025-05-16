be642df3133346efec7dcc29e944c72c9473bef9
sharkioso <tetkin04@mail.ru>
2025-05-16 21:32:08 +0500

    Создан интерфейс для хендлеров

---
+++ tgbot/src/main/java/com/example/Handlers/IHandle.java
@@ -0,0 +1,11 @@
+package com.example.Handlers;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public interface IHandle {
+    SendMessage handle(Update update);
+
+    String info();
+    String name();
+}
--- tgbot/src/main/java/com/example/bot/TelegramBot.java
+++ tgbot/src/main/java/com/example/bot/TelegramBot.java
@@ -3,6 +3,7 @@ package com.example.bot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class TelegramBot extends TelegramLongPollingBot {
 
@@ -14,16 +15,21 @@ public class TelegramBot extends TelegramLongPollingBot {
         SendMessage message = new SendMessage();
         message.setChatId(userId);
         message.setText(messageText);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            System.out.println("uncorrected"+e);
+        }
 
     }
 
     @Override
     public String getBotToken() {
-        return System.getenv("TelegramBotToken");
+        return System.getenv("TelegramBotNaumenToken");
     }
 
     @Override
     public String getBotUsername() {
-        return System.getenv("TelegramBotName");
+        return System.getenv("TelegramBotNaumenName");
     }
 }
