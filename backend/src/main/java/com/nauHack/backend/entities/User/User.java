453600dd55e5b64fc7cb54be2ff859c3e240c966
Vladislav Ostanin <ostanin8900@yandex.ru>
2025-05-16 20:47:25 +0500

    add jwt depen

--- backend/src/main/java/com/nauHack/backend/entities/User/User.java
+++ backend/src/main/java/com/nauHack/backend/entities/User/User.java
@@ -1,12 +1,23 @@
 package com.nauHack.backend.entities.User;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name = "roles")
+@Table(name = "users")
 public class User {
 
     @Id
@@ -15,7 +26,7 @@ public class User {
 
     @NotBlank
     @Size(max = 20)
-    private String username;
+    private String login;
 
     @NotBlank
     @Size(max = 50)
@@ -25,5 +36,9 @@ public class User {
     @NotBlank
     @Size(max = 120)
     private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
 }
