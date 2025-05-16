6f1f09303c44e8ced41202697c4d2528ea0bafe3
Vladislav Ostanin <ostanin8900@yandex.ru>
2025-05-16 23:06:33 +0500

    add requests

---
+++ backend/src/main/java/com/nauHack/backend/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.nauHack.backend.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nauHack.backend.repository.RoleRepository;
+import com.nauHack.backend.repository.UserRepository;
+import com.nauHack.backend.security.jwt.JwtUtils;
+import com.nauHack.backend.service.UserDetailsImpl;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    
+    
+}
