472d91a2c2e9bb2b313a2bea594ca36e6d97e904
Vladislav Ostanin <ostanin8900@yandex.ru>
2025-05-16 20:52:18 +0500

    add security

--- backend/src/main/java/com/nauHack/backend/repository/UserRepository.java
+++ backend/src/main/java/com/nauHack/backend/repository/UserRepository.java
@@ -1,5 +1,11 @@
 package com.nauHack.backend.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nauHack.backend.entities.User.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }
