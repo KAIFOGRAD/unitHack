652a4a1e62ea3a387f7523e490ec867707d2ca42
sharkioso <tetkin04@mail.ru>
2025-05-18 18:01:20 +0500

    исправлены баги

--- tgbot/src/main/java/com/example/bot/Executer.java
+++ tgbot/src/main/java/com/example/bot/Executer.java
@@ -1,33 +1,27 @@
 package com.example.bot;
 
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.generics.BotSession;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.springframework.stereotype.Component;
 
 @Component
 public class Executer {
-    private final TelegramBotsApi botApi;
-    private BotSession botSession;
-    private final TelegramBot bot;
+    private final AbsSender absSender;
 
-    public Executer(TelegramBot bot) throws TelegramApiException {
-        this.botApi = new TelegramBotsApi(DefaultBotSession.class);
-        this.bot = bot;
-        this.botSession = botApi.registerBot(bot);
+    public Executer(AbsSender absSender) {
+        this.absSender = absSender;
     }
 
-    public void sendMessage(String messageText, long userId) {
+    public void sendMessage(String text, Long chatId) {
         SendMessage message = new SendMessage();
-        message.setText(messageText);
-        message.setChatId(userId);
+        message.setChatId(chatId.toString());
+        message.setText(text);
 
         try {
-            bot.execute(message);
+            absSender.execute(message);
         } catch (TelegramApiException e) {
-            System.out.println("Error sending message: " + e.getMessage());
+            System.err.println("Ошибка отправки сообщения: " + e.getMessage());
         }
     }
 }
