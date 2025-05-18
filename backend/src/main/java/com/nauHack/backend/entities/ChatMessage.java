1ec528c7eb01781709ebeeb7b9edc596052ed181
Vladislav Ostanin <ostanin8900@yandex.ru>
2025-05-18 19:07:55 +0500

    add websocket entity

---
+++ backend/src/main/java/com/nauHack/backend/entities/ChatMessage.java
@@ -0,0 +1,37 @@
+package com.nauHack.backend.entities;
+
+import com.nauHack.backend.entities.User.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "chat_messages")
+@Getter
+@Setter
+@NoArgsConstructor
+public class ChatMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String content;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User sender;
+
+    public ChatMessage(String content, User sender) {
+        this.content = content;
+        this.sender = sender;
+    }
+}
